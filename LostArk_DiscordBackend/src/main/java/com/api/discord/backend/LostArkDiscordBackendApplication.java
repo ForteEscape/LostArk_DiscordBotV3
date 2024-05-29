a1a9764b7b7ab2c8b544a34af018f2721e922fe5
ForteEscape <sehun5515@naver.com>
2024-05-29 14:36:05 +0900

    init project

---
+++ LostArk_DiscordBackend/src/main/java/com/api/discord/backend/LostArkDiscordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.discord.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostArkDiscordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostArkDiscordBackendApplication.class, args);
+	}
+
+}
---
+++ LostArk_DiscordBackend/src/test/java/com/api/discord/backend/LostArkDiscordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.discord.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostArkDiscordBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
