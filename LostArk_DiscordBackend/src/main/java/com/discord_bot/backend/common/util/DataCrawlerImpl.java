a290335aef217d61d36f7b3963d2214c4535a395
ForteEscape <sehun5515@naver.com>
2024-05-29 20:48:16 +0900

    [ADD] add DataCrawler Implements

--- LostArk_DiscordBackend/src/main/java/com/discord_bot/backend/common/util/DataCrawlerImpl.java
+++ LostArk_DiscordBackend/src/main/java/com/discord_bot/backend/common/util/DataCrawlerImpl.java
@@ -3,8 +3,16 @@ package com.discord_bot.backend.common.util;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Component
 public class DataCrawlerImpl implements DataCrawler {
+
+    private static final String KEY_PREFIX = "Bearer ";
 
     @Value("${api.service-key}")
     private String SERVICE_KEY;
@@ -12,23 +20,70 @@ public class DataCrawlerImpl implements DataCrawler {
     @Value("${api.url.armories}")
     private String CHARACTER_ARMOR_URL;
 
+    @Value("${api.url.characters}")
+    private String CHARACTER_URL;
+
+    @Value("${api.url.news}")
+    private String NEWS_URL;
+
     @Override
     public String getCharacterInfo(String characterName) {
-        return "";
+        String urlString = CHARACTER_ARMOR_URL + "?characterName=" + characterName;
+
+        return parsingData(urlString);
     }
 
     @Override
     public String getCharacterGroup(String characterName) {
-        return "";
+        String urlString = CHARACTER_URL + "?characterName=" + characterName;
+
+        return parsingData(urlString);
     }
 
     @Override
     public String getGameNotices(String searchTitle, String noticeType) {
-        return "";
+        String urlString = NEWS_URL + "?searchTitle=" + searchTitle + "&noticeType=" + noticeType;
+
+        return parsingData(urlString);
     }
 
     @Override
     public String getGameEvents() {
         return "";
+    }
+
+    private String parsingData(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("ContentType", "application/json");
+            conn.setRequestProperty("Authorization", KEY_PREFIX + SERVICE_KEY);
+
+            int responseCode = conn.getResponseCode();
+
+            BufferedReader br;
+
+            if (responseCode >= 200 && responseCode < 300) {
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            br.close();
+            conn.disconnect();
+
+            return sb.toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
