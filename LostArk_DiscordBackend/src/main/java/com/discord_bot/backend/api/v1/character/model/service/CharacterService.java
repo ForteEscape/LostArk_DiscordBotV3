093f6cec706ae5426ccebcf9ffe2768ae0ca6d7d
ForteEscape <sehun5515@naver.com>
2024-05-29 23:14:29 +0900

    [REMOVE] remove test controller

--- LostArk_DiscordBackend/src/main/java/com/discord_bot/backend/api/v1/character/model/service/CharacterService.java
+++ LostArk_DiscordBackend/src/main/java/com/discord_bot/backend/api/v1/character/model/service/CharacterService.java
@@ -1,4 +1,6 @@
 package com.discord_bot.backend.api.v1.character.model.service;
 
 public interface CharacterService {
+
+    void test();
 }
--- LostArk_DiscordBackend/src/main/java/com/discord_bot/backend/api/v1/character/model/service/CharacterServiceImpl.java
+++ LostArk_DiscordBackend/src/main/java/com/discord_bot/backend/api/v1/character/model/service/CharacterServiceImpl.java
@@ -1,14 +1,25 @@
 package com.discord_bot.backend.api.v1.character.model.service;
 
 import com.discord_bot.backend.api.v1.character.model.mapper.CharacterMapper;
+import com.discord_bot.backend.api.v1.common.util.DataCrawler;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CharacterServiceImpl implements CharacterService {
 
     private final CharacterMapper characterMapper;
+    private final DataCrawler dataCrawler;
 
+    @Override
+    public void test() {
+        String result = dataCrawler.getGameNotices("", new String("공지".getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8));
 
+        log.info("dataCrawl result : " + result);
+    }
 }
